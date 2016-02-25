74a8587b822bb14e9e37144ec5bcb3106224c18b
Paulo J. S. Silva <pjssilva@gmail.com>
2016-02-25 15:22:05 -0300

    Pequenas correcoes nos textos de ponto flutuante e sistemas lineares. Mudancas para permitir usar o juliabox que não permite Unicode nos nomes de arquivos.

--- Makefile
+++ Makefile
@@ -23,6 +23,30 @@ clean :
 cleanall : clean
 	-rm -r $(PDFS)
 
-# Regra para gerar o pdf a partir de um caderno
-%.pdf : %.ipynb
-	python converte_para_pdf.py $<
+# Regra para gerar os pdfs a partir dos respectivos cadernos
+Introdução.pdf : Introducao.ipynb
+	python converte_para_pdf.py $@
+
+Computação_com_Precisão_Finita.pdf : Computacao_com_Precisao_Finita.ipynb
+	python converte_para_pdf.py $@
+
+Sistemas_Lineares.pdf : Sistemas_Lineares.ipynb
+	python converte_para_pdf.py $@
+
+Equações_Não-Lineares.pdf : Equacoes_Nao-Lineares.ipynb
+	python converte_para_pdf.py $@
+
+Quadrados_Mínimos.pdf : Quadrados_Minimos.ipynb
+	python converte_para_pdf.py $@
+
+Interpolação_Polinomial.pdf : Interpolacao_Polinomial.ipynb
+	python converte_para_pdf.py $@
+
+Integração_Numérica.pdf : Integracao_Numerica.ipynb
+	python converte_para_pdf.py $@
+
+Problemas_de_Valor_Inicial.pdf : Problemas_de_Valor_Inicial.ipynb
+	python converte_para_pdf.py $@
+
+Problemas_de_Valor_de_Contorno.pdf: Problemas_de_Valor_de_Contorno.ipynb
+	python converte_para_pdf.py $@
--- converte_para_pdf.py
+++ converte_para_pdf.py
@@ -7,21 +7,29 @@ bem com nomes de arquivo que contem espaco.
 
 import sys
 import shutil
+import subprocess
 import os
+import unicodedata
+
+
+def remove_acentos(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
+
 
 # Comando para chamar nbconvert e suas opcoes.
-opcoes = " --execute --config custom_cfg.py --template notas.tplx"
-opcoes += " --to pdf"
-comando = "jupyter nbconvert" + opcoes
+comando = u"jupyter nbconvert --execute --allow-errors --config custom_cfg.py"
+comando += u" --template notas.tplx --to pdf"
 
 # Pega o nome de arquivo e troca sublinha por espaco.
-nome_arquivo = sys.argv[1][:-6]
-nome_com_espacos = nome_arquivo.replace('_', ' ')
+nome_base = sys.argv[1].decode("utf-8")[:-4]
+nome_sem_acentos = remove_acentos(nome_base)
+nome_com_espacos = nome_base.replace('_', ' ')
 
 # Executa o comando usando um arquivo com nome adequado
-shutil.copy(nome_arquivo + ".ipynb", nome_com_espacos + ".ipynb")
-comando += ' "%s"' % (nome_com_espacos + ".ipynb")
+shutil.copy(nome_sem_acentos + ".ipynb", nome_com_espacos + ".ipynb")
+comando += u' "%s"' % (nome_com_espacos + ".ipynb")
 print comando
-os.system(comando)
+subprocess.call(comando, shell=True)
 os.unlink(nome_com_espacos + ".ipynb")
-os.rename(nome_com_espacos + ".pdf", nome_arquivo + ".pdf")
+os.rename(nome_com_espacos + ".pdf", nome_base + ".pdf")

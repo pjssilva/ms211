c4365a2b194253dcb6513c5320fb34c659b7eb66
Paulo J. S. Silva <pjssilva@gmail.com>
2016-02-25 18:44:41 -0300

    Ajustes em sistemas lineares, principais correções ortográficas.

--- converte_para_pdf.py
+++ converte_para_pdf.py
@@ -16,20 +16,23 @@ def remove_acentos(s):
     return ''.join(c for c in unicodedata.normalize('NFD', s)
                    if unicodedata.category(c) != 'Mn')
 
-
-# Comando para chamar nbconvert e suas opcoes.
-comando = u"jupyter nbconvert --execute --allow-errors --config custom_cfg.py"
-comando += u" --template notas.tplx --to pdf"
-
 # Pega o nome de arquivo e troca sublinha por espaco.
 nome_base = sys.argv[1].decode("utf-8")[:-4]
 nome_sem_acentos = remove_acentos(nome_base)
 nome_com_espacos = nome_base.replace('_', ' ')
 
 # Executa o comando usando um arquivo com nome adequado
+# Comando para chamar nbconvert e suas opcoes.
+comando = u"jupyter nbconvert --execute --allow-errors --config custom_cfg.py"
+comando += u" --template notas.tplx --to pdf"
 shutil.copy(nome_sem_acentos + ".ipynb", nome_com_espacos + ".ipynb")
 comando += u' "%s"' % (nome_com_espacos + ".ipynb")
 print comando
 subprocess.call(comando, shell=True)
+comando = u"jupyter nbconvert --template notas.tplx --to pdf"
+comando += u' "%s"' % (nome_com_espacos + ".ipynb")
+print comando
+subprocess.call(comando, shell=True)
+
 os.unlink(nome_com_espacos + ".ipynb")
 os.rename(nome_com_espacos + ".pdf", nome_base + ".pdf")
